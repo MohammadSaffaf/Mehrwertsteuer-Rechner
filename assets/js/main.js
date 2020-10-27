56a8b360cf3edc66e1f80a74604851d5c0802d14
MohammadSaffaf <mhmdsaffha@gmail.com>
2020-10-27 17:01:43 +0100

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,26 +9,23 @@ function myFunction2() {
     document.getElementById("ergebnis-2").innerHTML="Nettobetrag"
 }
 function Berechnen() {
-    var numPrice = document.getElementById("numPrice").value;
-    let multiplikator = document.getElementById("netto");
-    let multiplikator2 = document.getElementById("brutto");
-    var x = document.getElementById("prozent-1").checked ? .19 : .07;
+    let numPrice = document.getElementById("numPrice").value;
+    let netto = document.getElementById("netto");
+    let brutto = document.getElementById("brutto");
+    var prozent = document.getElementById("prozent-1").checked ? .19 : .07;
     var steuer = ''
-    if (multiplikator.checked) {
-        steuer = numPrice * x;
-        var endsumme = Number(steuer) + Number(numPrice)
+    if (netto.checked) {
+        steuer = numPrice * prozent;
+        var result = Number(steuer) + Number(numPrice)
 
-        document.getElementById("müll").innerHTML = `<p> ${Number(steuer.toFixed(2))} €</p>`;
-    document.getElementById("müll2").innerHTML = `<p> ${Number(endsumme.toFixed(2))} €</p>`;
-    } else if (multiplikator2.checked) {
-        steuer = numPrice / (1 + x);
-        var endsumme = Number(numPrice) - Number(steuer);
-        document.getElementById("müll").innerHTML = `<p> ${Number(endsumme.toFixed(2))} €</p>`;
-    document.getElementById("müll2").innerHTML = `<p> ${Number(steuer.toFixed(2))} €</p>`;
+        document.getElementById("Ergebnis-h2").innerHTML = ` ${Number(steuer.toFixed(3))} €`;
+    document.getElementById("Ergebnis-h3").innerHTML = ` ${Number(result.toFixed(3))} €`;
+    } else if (brutto.checked) {
+        steuer = numPrice / (1 + prozent);
+        var result = Number(numPrice) - Number(steuer);
+        document.getElementById("Ergebnis-h2").innerHTML = ` ${Number(result.toFixed(2))} €`;
+    document.getElementById("Ergebnis-h3").innerHTML = ` ${Number(steuer.toFixed(2))} €`;
         
     }
-    
-    
-    
-    
+      
 }
